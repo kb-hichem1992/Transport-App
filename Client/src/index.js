ba88abc8534304ba64787451b69d3664b5eb5cce
kb-hichem1992 <kb-hichem@hotmail.fr>
2021-07-05 00:57:52 +0100

    deleting code of auth0 from the index.js

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,23 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Auth0Provider } from "@auth0/auth0-react";
-import Home from "./Home.js";
+
 import App from "./App";
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
-
-
-ReactDOM.render(
-/*   <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}
-  >
-      <Home />
-  </Auth0Provider>, */
-  <App />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
