4d98dcd531e9a58f0ad47d2e217ce98e8e9db096
ali braiki <alialibr64@gmail.com>
2021-07-07 01:57:56 +0200

    Diplome gener modification

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const mysql = require("mysql");
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "root",
   database: "transport",
   dateStrings:true,
 });
