83a3facab8d7e98b2a2f399622e1e4d8ae88ba8c
kb-hichem1992 <kb-hichem@hotmail.fr>
2021-07-12 23:08:50 +0100

    commiting nothing

--- Server/index.js
+++ Server/index.js
@@ -343,53 +343,60 @@ app.delete("/delete_formation/:numeroFormation/:numeroAgrement", (req, res) => {
 //=======
 
 // DIPLOME GENERATION BY ID_INSC ID_FORMATION  ID_PERMIS DATE_INS NUM_AGR FROM PASSE TABLE
-app.get("/report/DIPLOME/:idin/:idform/:idperm/:dateins/:numagr", (req, res) => {
-//>>>>>>> 46cf087b894eb3ed2168f77f3c5e1a9b36b8797e
+app.get(
+  "/report/DIPLOME/:idin/:idform/:idperm/:dateins/:numagr",
+  (req, res) => {
+    //>>>>>>> 46cf087b894eb3ed2168f77f3c5e1a9b36b8797e
 
-var fullUrl = req.protocol + '://' + req.get('host');
-  
-  
-  const idin = req.params.idin;
-  const idform=req.params.idform;
-  const idperm=req.params.idperm;
-  const dateins=req.params.dateins;
-  const numagr=req.params.numagr;
+    var fullUrl = req.protocol + "://" + req.get("host");
 
+    const idin = req.params.idin;
+    const idform = req.params.idform;
+    const idperm = req.params.idperm;
+    const dateins = req.params.dateins;
+    const numagr = req.params.numagr;
 
-  pdf.generatepdf(idin,idform,idperm,dateins,numagr,fullUrl,function(dt){
-    //console.log(dt);
-    res.send(dt);
-  });
-
- 
-   
-
-});
-
-
-
-
+    pdf.generatepdf(
+      idin,
+      idform,
+      idperm,
+      dateins,
+      numagr,
+      fullUrl,
+      function (dt) {
+        //console.log(dt);
+        res.send(dt);
+      }
+    );
+  }
+);
 
 // EVALUATION GENERATION BY ID_INSC ID_FORMATION  ID_PERMIS DATE_INS NUM_AGR FROM PASSE TABLE
-app.get("/report/EVALUATION/:idin/:idform/:idperm/:dateins/:numagr", (req, res) => {
-//>>>>>>> 46cf087b894eb3ed2168f77f3c5e1a9b36b8797e
+app.get(
+  "/report/EVALUATION/:idin/:idform/:idperm/:dateins/:numagr",
+  (req, res) => {
+    //>>>>>>> 46cf087b894eb3ed2168f77f3c5e1a9b36b8797e
 
-var fullUrl = req.protocol + '://' + req.get('host');
-  
-  
-  const idin = req.params.idin;
-  const idform=req.params.idform;
-  const idperm=req.params.idperm;
-  const dateins=req.params.dateins;
-  const numagr=req.params.numagr;
+    var fullUrl = req.protocol + "://" + req.get("host");
 
+    const idin = req.params.idin;
+    const idform = req.params.idform;
+    const idperm = req.params.idperm;
+    const dateins = req.params.dateins;
+    const numagr = req.params.numagr;
 
-
- pdf.generatepdf2(idin,idform,idperm,dateins,numagr,fullUrl,function(dt){
-    //console.log(dt);
-    res.send(dt);
-  });
-   
+    pdf.generatepdf2(
+      idin,
+      idform,
+      idperm,
+      dateins,
+      numagr,
+      fullUrl,
+      function (dt) {
+        //console.log(dt);
+        res.send(dt);
+      }
+    );
 
     const idin = req.params.idin;
     const idform = req.params.idform;
@@ -399,20 +406,6 @@ var fullUrl = req.protocol + '://' + req.get('host');
     pdf.generatepdf(idin, idform, idperm, dateins, numagr, fullUrl);
   }
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.post("/Add_passe", (req, res) => {
   const numeroCandidat = req.body.numeroCandidat;
