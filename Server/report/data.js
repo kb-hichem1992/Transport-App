77394bdb16b13486279bb34447cc344c507a1477
ali braiki <alialibr64@gmail.com>
2021-07-23 21:29:28 +0200

    requete sql update

--- Server/report/data.js
+++ Server/report/data.js
@@ -51,14 +51,12 @@ mq.query( `SELECT NOM_CANDIDAT,PRENOM_CANDIDAT,DATE_NAIS_CANDIDAT,LIEU_NAIS_CAND
 
 
 
-function GetEvaluationData(NumIns,NumPerm,DateIns,NumAGR,callback)
+function GetEvaluationData(NumIns,NumPerm,DateIns,callback)
 {
 
 
-mq.query( `SELECT NOM_CANDIDAT,PRENOM_CANDIDAT,DATE_NAIS_CANDIDAT,LIEU_NAIS_CANDIDAT,ADRESSE_CANDIDAT,passe.DATE_INS,passe.NUM_INS
- FROM candidat,passe WHERE passe.NUM_INS=?  and passe.NUM_PERMIS=? and passe.DATE_INS=?
- AND passe.NUMERO_AGREMENT=? AND passe.DATE_INS=candidat.DATE_INS
- AND passe.NUM_INS=candidat.NUM_INS AND passe.NUM_PERMIS=candidat.NUM_PERMIS  `,[NumIns,NumPerm,DateIns,NumAGR],
+mq.query( `SELECT NOM_CANDIDAT,PRENOM_CANDIDAT,DATE_NAIS_CANDIDAT,LIEU_NAIS_CANDIDAT,ADRESSE_CANDIDAT,DATE_INS,NUM_INS
+ FROM candidat WHERE NUM_INS=?  and NUM_PERMIS=? and DATE_INS=?  `,[NumIns,NumPerm,DateIns],
  (err, result) => {
     
     if (err) throw err;
